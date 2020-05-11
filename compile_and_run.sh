5351b8d999b6cdbbd5040eeedcc7cea2179d5b83
encore64 <mikkel@undone.se>
2020-05-11 12:45:29 +0200

    Updated build bash-file.

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -3,14 +3,14 @@
 # remove previous build
 rm -f ./build/s10-syx2wav
 
-# compile program
+# compile program for linux
 gcc main.c -o ./build/s10-syx2wav
 
-# run program with example file
+# example for compiling for windows (32 and 64 bit compatible)
+# i686-w64-mingw32-gcc main.c -o ./build/s10-syx2wav.exe
 
-#./build/s10-syx2wav "./example-files/L-100 001 A Drum Set (BD).syx" "./example-files/L-100 001 A Drum Set (BD).wav"
+# run program with example file
 find "./example-files/" -type f -name "*.syx" | while read fname; do
 	echo "processing: $fname"
-	./build/s10-syx2wav "$fname" "$fname.wav"
+	./build/s10-syx2wav "$fname"
 done
-
